2b68146c37672e466f6f03ffe6260aac24e65929
Chaz Coats <chazcoats2@hotmail.com>
2023-08-22 15:18:17 -0400

    Working out HTML routes on server file

--- Develop/server.js
+++ Develop/server.js
@@ -3,3 +3,16 @@ var fs = require("fs");
 var path = require("path");
 var PORT = process.env.PORT || 3001;
 var app = express();
+var apiRoute = require("./routes/apiRoute");
+var htmlRoute = require("./routes/htmlRoute");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
+
+app.use("/", htmlRoute);
+
+app.listen(PORT, () => {
+    console.log(`Example app listening at http://localhost:${PORT}`)
+});
