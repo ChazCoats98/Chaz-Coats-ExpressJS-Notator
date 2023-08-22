95f5c9c2bc35c36a741cb64c83b9ecebebd9b546
Chaz Coats <chazcoats2@hotmail.com>
2023-08-22 15:29:55 -0400

    Working on API route

--- Develop/server.js
+++ Develop/server.js
@@ -3,16 +3,20 @@ var fs = require("fs");
 var path = require("path");
 var PORT = process.env.PORT || 3001;
 var app = express();
-var apiRoute = require("./routes/apiRoute");
-var htmlRoute = require("./routes/htmlRoute");
+var api = require("./routes/apiRoute");
+var html = require("./routes/htmlRoute");
 
+//these functions are used to parse JSON and url encoded data 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+//function to host the public folder as a static resource
 app.use(express.static("public"));
 
-app.use("/", htmlRoute);
-
+//These functions connect routes to the links clicked on the page
+app.use("/", html);
+app.use("/api", api);
+//This initializes the server 
 app.listen(PORT, () => {
     console.log(`Example app listening at http://localhost:${PORT}`)
 });
